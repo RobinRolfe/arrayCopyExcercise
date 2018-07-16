75b6876b109285cb5a5cc28f51b262897bc79270
ROBIN ALEXANDER ROLFE <S14001664@mail.glyndwr.ac.uk>
2018-07-16 22:05:20 +0100

    Add project files.

---
+++ arrayCopyExcercise/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrayCopyExcercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] firstArray = { 1, 2, 3, 4, 5 };
+            int[] secondArray = new int[5];
+
+            for (int i = 0; i < firstArray.Length; i++)
+            {
+                secondArray[i] = firstArray[i];
+            }
+
+
+
+            Console.Write("first array = ");
+            for (int i = 0; i < secondArray.Length; i++)
+            {
+                Console.Write( firstArray[i] + ", ");
+            }
+
+
+            Console.WriteLine("");
+            Console.Write( "second array = ");
+            for (int i = 0; i < secondArray.Length; i++)
+            {
+                Console.Write(  secondArray[i] + ", ");
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
