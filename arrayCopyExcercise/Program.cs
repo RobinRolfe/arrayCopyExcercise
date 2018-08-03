b16c26d3c65e1ef13dcf260128a9665ef11cfadc
Robin Rolfe <s14001664@mail.glyndwr.ac.uk>
2018-08-03 07:34:47 +0100

    Update Program.cs
    Added comments

--- arrayCopyExcercise/Program.cs
+++ arrayCopyExcercise/Program.cs
@@ -10,17 +10,19 @@ namespace arrayCopyExcercise
     {
         static void Main(string[] args)
         {
-
-            int[] firstArray = { 1, 2, 3, 4, 5 };
-            int[] secondArray = new int[5];
-
+            
+            int[] firstArray = { 1, 2, 3, 4, 5 };//Declare an arrays with values
+            int[] secondArray = new int[5];////Declare an empty array
+            
+            
+            //copy the array using a for loop
             for (int i = 0; i < firstArray.Length; i++)
             {
                 secondArray[i] = firstArray[i];
             }
 
 
-
+            //Display the results
             Console.Write("first array = ");
             for (int i = 0; i < secondArray.Length; i++)
             {
